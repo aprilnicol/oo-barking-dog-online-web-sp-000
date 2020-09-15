220b959573975268932360e1641e8506f7064797
April Adams <illusionlace@gmail.com>
2020-09-15 01:04:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 # Your code goes here!
 class Dog
   def .name=(dogs_name)
-  end 
-  def .name 
+  end
+  def .name
     puts "#{.name}"
-  end 
-  def bark 
+  end
+  def bark
     puts "woof!"
-  end 
+  end
   
